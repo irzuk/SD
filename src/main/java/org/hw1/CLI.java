4344dc22fa78ee3477d60fddb9ac84c3c47dd039
mkornaukhov03 <mkornaukhov03@yandex.ru>
2023-02-26 02:29:42 +0300

    Implement Lexer

--- src/main/java/org/hw1/CLI.java
+++ src/main/java/org/hw1/CLI.java
@@ -2,30 +2,41 @@ package org.hw1;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CLI {
     Lexer lexer;
     Parser parser;
     CmdManager cmdManager;
 
+
+    private static void printPrompt(PrintStream os) {
+        os.print("user@machine:/cur/dir$ ");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
-//        while (true) {
-            // need input stream
-            // System.in.read();
-
-            // give it to lexer
-            // org.hw1.Lexer.getTokens();
-
-            // pass tokens to parser
-            // org.hw1.Parser.getCommandsFromTokens();
-            // + detect exit()
-
-            // pass commands to cmdManager
-            // OutputStream out = org.hw1.CmdManager.startPipline();
-            // getPipeOutputStream to System.out.print();
-//        }
+        PrintStream out = System.out;
+        InputStream in = System.in;
+        Lexer l = new Lexer(System.in);
+        while (true) {
+            printPrompt(out);
+            List<Token> resp = l.getTokens();
+            out.println("Get cmd:");
+            for (var item : resp) {
+                out.println(item);
+            }
+            out.println("-- end of cmd");
+//            out.println("");
+//             give it to lexer
+//             org.hw1.Lexer.getTokens();
+//
+//             pass tokens to parser
+//             org.hw1.Parser.getCommandsFromTokens();
+//             + detect exit()
+//
+//             pass commands to cmdManager
+//             OutputStream out = org.hw1.CmdManager.startPipline();
+//             getPipeOutputStream to System.out.print();
+        }
     }
 
 }
