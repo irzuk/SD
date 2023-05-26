5fe2755bf7cfa6e70b52b06272fe51ec6c5b65e5
Deuqz <ddemon2002@mail.com>
2023-05-26 05:31:44 +0300

    Fix EnemyElement

--- src/main/java/org/Roguelike/collections/map/elements/EnemyElement.java
+++ src/main/java/org/Roguelike/collections/map/elements/EnemyElement.java
@@ -1,4 +1,11 @@
 package org.Roguelike.collections.map.elements;
 
+import static org.Roguelike.collections.map.MapElementsParameters.ENEMY_HEIGHT;
+import static org.Roguelike.collections.map.MapElementsParameters.ENEMY_WIDTH;
+
 public class EnemyElement extends MapElement {
+    static {
+        WIDTH = ENEMY_WIDTH;
+        HEIGHT = ENEMY_HEIGHT;
+    }
 }
